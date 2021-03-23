8a4f620a945f66a0f26c9ff67bc5eccacc4b9c42
蔡月艳 <1105566029@qq.com>
2021-03-23 20:37:48 +0800

    扫雷

---
+++ 3-22/3-22/源test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"game.h"
+//测试扫雷游戏的 9*9
+void menu()
+{
+	printf("********************\n");
+	printf("*****  1.play  *****\n");
+	printf("*****  0.exit  *****\n");
+	printf("********************\n");
+
+}
+
+
+void game()
+{
+	char mine[ROWS][COLS];//存放雷的信息
+	char show[ROW][COL];//排查出放雷的信息
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	DisplayBoard(show, ROW, COL);
+	//DisplayBoard(mine, ROW, COL);
+	//1.布置雷
+	SetMine(mine,ROW,COL);
+	//2.排查雷
+	FindMine(mine,show,ROW, COL);
+
+
+}
+
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，重新选择\n");
+			break;
+		}
+	} while (input);
+
+}
+
+
+int main()
+{
+	test();
+	return 0;
+}
